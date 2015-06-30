1b5a236fdc32ba5b5ed5fa101c5a8d7caed4c9ce
Walter C <wachen@mozilla.com>
2015-06-30 17:12:53 +0800

    SQLite self test

--- sqlite.py
+++ sqlite.py
@@ -12,10 +12,10 @@ class LogDatabase():
             os.mkdir("db/")
 
         ts = time.time()
-        conn = sqlite3.connect("db/" + datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S') + ".db")
+        self.conn = sqlite3.connect("db/" + datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S') + ".db")
         print "Opened database successfully"
 
-        conn.execute('''CREATE TABLE COMPANY
+        self.conn.execute('''CREATE TABLE LOG
            (ID             INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,
             HOST           VARCHAR(32)    NOT NULL,
             SERIAL         VARCHAR(32)    NOT NULL,
@@ -26,9 +26,23 @@ class LogDatabase():
             TIMESTAMP      DATETIME       DEFAULT CURRENT_TIMESTAMP);''')
 
         print "Table created successfully"
+        self.cur = self.conn.cursor()
+
+    def insert(self, host, serial, status, typ, availability, build):
+        self.cur.execute("INSERT INTO log(host,serial,status,type,availability,build) VALUES('" + host + "', '" + serial + "', '" + status + "', '" + typ + "', '" + availability + "', '" + build + "')")
+ 
+    def get_posts(self):
+        self.cur.execute("SELECT * FROM LOG")
+        print(self.cur.fetchall())
 
     def close(self):
-        conn.close()
+        print "Closing database"
+        self.conn.close()
 
 if __name__ == "__main__":
+    # This is a basic test for databse
     db = LogDatabase()
+    for x in range(10):
+        db.insert('a', 'a', 'a', 'a', 'a', 'a')
+    db.get_posts()
+    db.close()
